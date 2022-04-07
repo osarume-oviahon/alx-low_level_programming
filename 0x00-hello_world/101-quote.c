23d0870f3aa62a1f0d7f1b55952fe0ec1bc45d30
Your Name <you @ example.com>
2022-04-07 15:36:12 -0700

    Task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
   * main - Entry point
+  *
   * Return: Always 1 (success)
   */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
