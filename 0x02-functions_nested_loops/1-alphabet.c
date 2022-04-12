d3b25a27cf5ba5249f0dd678b91be7e1b57eab83
Your Name <you @ example.com>
2022-04-12 10:47:32 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-  * main - print alphabet lowercase
+  * print_alphabet - print alphabet lowercase
   *
   * Return: Always 0.
   */
 
-int main(void)
+void print_alphabet(void)
 {
 	char alphabet;
 
