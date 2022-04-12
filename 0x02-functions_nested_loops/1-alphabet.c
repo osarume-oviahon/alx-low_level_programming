30f17686ee099654e9d0a038361ee3006871d3f0
Your Name <you @ example.com>
2022-04-12 10:36:38 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - print alphabet lowercase
+  *
+  * Return: Always 0.
+  */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
