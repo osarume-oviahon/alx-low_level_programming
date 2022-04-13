654718dbcc38ae0aeeb5dc6ad4aa1917df164bd8
Your Name <you @ example.com>
2022-04-13 02:16:19 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * times_table - Prints the times table of the input,
+ * print_times_table - Prints the times table of the input,
  *                     starting with 0.
  * @n: The value of the times table to be printed.
  */
-void times_table(int n)
+void print_times_table(int n)
 {
 int num, mult, prod;
 
