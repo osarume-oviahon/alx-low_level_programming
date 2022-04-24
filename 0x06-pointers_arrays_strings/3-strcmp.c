a41d735825eb8f1565ea912f02f9a683d0861072
Your Name <you @ example.com>
2022-04-24 13:19:02 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strcmp - compares two strings
+ *@s1: A pointer to the value of a character that will be changed
+ *@s2: A pointer to the value of a character that will also be reassigned
+ *Return: dest
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+	char a, b;
+
+	while (a != '\0' && b != '\0' && a == b)
+	{
+		a++;
+		b++;
+	}
+
+	return (a - b)
+
+}
