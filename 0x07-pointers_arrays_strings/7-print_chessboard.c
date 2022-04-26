7c340ecbe9d6c6a0cba6bc19c5af5618b2ad7208
Your Name <you @ example.com>
2022-04-26 15:58:02 -0700

    Task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the sum of the two diagonals of a square matrix of integers
+ * @a: 2d array of chars
+ *Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int arr1, arr2;
+
+	arr1 = 0;
+	while (arr1 < 8)
+	{
+		arr2 = 0;
+		while (arr2 < 8)
+		{
+			_putchar (a[arr1][arr2]);
+			arr2++;
+		}
+		_putchar ('\n');
+		arr1++;
+	}
+
+}
