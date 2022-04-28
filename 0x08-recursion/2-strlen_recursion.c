473c4d9858ffe52d79f2ef9f7a5980ed7f019096
Your Name <you @ example.com>
2022-04-28 13:05:38 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen_recursion - returns the length of a string
+ *@s: pointer
+ *Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	char *s[] = "Length of the string";
+	int i;
+
+	for (i = 0; *s[i] != '\0'; ++i);
+
+	_putchar(*s);
+		return (0);
+
+}
