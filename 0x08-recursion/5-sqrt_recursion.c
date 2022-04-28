5221e60cf5da1bddd5a7e98629ab27a1ba4d19a0
Your Name <you @ example.com>
2022-04-28 14:02:11 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *@n: interger
+ *
+ *Return: 0
+ */
+
+int _evaluate(int a, int n)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	else if (a * a < n)
+		return (_evaluate(a + 1, n));
+	else if (a * a == n)
+		return (a);
+	return (-1);
+}
+
+int _sqrt_recursion(int n)
+{
+	int a = 0;
+	if (a < 0)
+		return (-1);
+	else
+	{
+		return (_evaluate(a, n));
+	}
+}
