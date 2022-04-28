86ff886b7ba76903342e0cf0dd83479d2788ce55
Your Name <you @ example.com>
2022-04-28 03:13:31 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,30 +1,31 @@
 #include "main.h"
 
 /**
-  * jack_bauer - prints every minute of the day
-  *
-  * Return: Always 0.
-  */
+ * jack_bauer -prints every minute of the day
+ *
+ * Return: Always 0.
+ */
 
-void jack_bauer(void);
+void jack_bauer(void)
+
 {
-int a, b;
+int a;
+int b;
 
-	for (a = 0; <= 23; a++)
-	{
-	for (b = 0; <= 59; b++)
-	{
-		_putchar (a / 10 + '0');
-		_putchar (a % 10 + '0');
-		_putchar (':');
-		_putchar (b / 10 + '0');
-		_putchar (b % 10 + '0');
-		_putchar ('\n');
-		return (0);
+for (a = 0; a <= 23; a++)
+{
+for (b = 0; b <= 59; b++)
+{
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
+_putchar (':');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
+_putchar ('\n');
 
-	}
+}
 
 
-	}
+}
 
 }
