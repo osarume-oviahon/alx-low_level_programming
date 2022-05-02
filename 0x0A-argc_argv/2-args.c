fc1efa52cb999dfe96527401d8fa65e235ab8326
Your Name <you @ example.com>
2022-05-02 10:46:35 -0700

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Pointer that holds the array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0;
+
+	while (argc > 0)
+
+	{
+		if (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+
+	return (0);
+}
